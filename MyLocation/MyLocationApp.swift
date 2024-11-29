5aa6b5ac37302f65d43db073d6bfde1a12b58aaf
Ronaldo-Akira <ronaki.kinoshita@gmail.com>
2024-11-28 21:54:31 -0300

    Initial Commit

---
+++ MyLocation/MyLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLocationApp.swift
+//  MyLocation
+//
+//  Created by Ronaldo Akira Fujigaki Kinoshita on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
